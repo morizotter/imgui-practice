98e844da1e055f56730b74967a29960065485af9
Naoki Morita <namorit@gmail.com>
2016-12-02 03:45:59 +0900

    Setup OpenGL

--- ImguiPractice/ImguiPractice/main.cpp
+++ ImguiPractice/ImguiPractice/main.cpp
@@ -8,6 +8,8 @@
 
 #include <iostream>
 #include <string>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
 #include <opencv2/opencv.hpp>
 #include "imgui.h"
 
@@ -15,26 +17,187 @@ using namespace std;
 using namespace cv;
 
 string win_name = "Window";
+string video_name = "video1.mp4";
+const char* vertex_file_name = "simple.vert";
+const char* fragment_file_name = "simple.frag";
 
-int main(int argc, const char * argv[]) {
-    
-    VideoCapture cap(0);
-    
-    namedWindow(win_name);
-    
-    while (1) {
-        Mat frame;
-        cap >> frame;
-        
-        imshow(win_name, frame);
-        
-        int key = waitKey(1);
-        if(key==27) {
-            break;
-        }
+#include <stdio.h>
+#include <stdlib.h>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+GLFWwindow* window;
+
+#include "shader.hpp"
+
+int main( void ) {
+    VideoCapture capture(video_name);
+    if (!capture.isOpened()) {
+        cerr << "Failed to open the video device, video file or image sequence!\n" << endl;
+        return 0;
     }
     
-    destroyWindow(win_name);
+    int width = capture.get(CV_CAP_PROP_FRAME_WIDTH);
+    int height = capture.get(CV_CAP_PROP_FRAME_HEIGHT);
+    printf("video size %d %d\n", width, height);
+    
+    // Initialise GLFW
+    if( !glfwInit() ) {
+        fprintf( stderr, "Failed to initialize GLFW\n" );
+        getchar();
+        return -1;
+    }
+    
+    glfwWindowHint(GLFW_SAMPLES, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // To make MacOS happy; should not be needed
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    
+    // Open a window and create its OpenGL context
+    window = glfwCreateWindow(width, height, "ImGui", NULL, NULL);
+    if( window == NULL ){
+        fprintf( stderr, "Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible. Try the 2.1 version of the tutorials.\n" );
+        getchar();
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    
+    // Initialize GLEW
+    glewExperimental = true; // Needed for core profile
+    if (glewInit() != GLEW_OK) {
+        fprintf(stderr, "Failed to initialize GLEW\n");
+        getchar();
+        glfwTerminate();
+        return -1;
+    }
+    
+    // Ensure we can capture the escape key being pressed below
+    glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+    
+    // Dark blue background
+    glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
+    
+    GLuint VertexArrayID;
+    glGenVertexArrays(1, &VertexArrayID);
+    glBindVertexArray(VertexArrayID);
+    
+    GLuint programID = LoadShaders(vertex_file_name, fragment_file_name);
+    
+    // テクスチャを準備する
+    GLuint Texture;
+    glGenTextures(1, &Texture);
+    glBindTexture(GL_TEXTURE_RECTANGLE, Texture);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+
+    glTexImage2D(GL_TEXTURE_2D, 0,GL_RGB, width, height, 0, GL_BGR, GL_UNSIGNED_BYTE, 0);
+    glGenerateMipmap(GL_TEXTURE_2D);
+    
+    GLuint TextureID  = glGetUniformLocation(programID, "textureSampler");
+    
+    static const GLfloat g_vertex_buffer_data[] = {
+        -1.0f, -1.0f, 0.0f,
+        1.0f, -1.0f, 0.0f,
+        1.0f,  1.0f, 0.0f,
+        -1.0f,  1.0f, 0.0f,
+    };
+    
+    // Two UV coordinatesfor each vertex.
+    static const GLfloat g_uv_buffer_data[] = {
+        0.0f, 1.0f,
+        1.0f, 1.0f,
+        1.0f, 0.0f,
+        0.0f, 0.0f
+    };
+    
+    GLuint vertexbuffer;
+    glGenBuffers(1, &vertexbuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
+    
+    GLuint uvbuffer;
+    glGenBuffers(1, &uvbuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, uvbuffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(g_uv_buffer_data), g_uv_buffer_data, GL_STATIC_DRAW);
+    
+    do{
+        Mat frame;
+        
+        capture >> frame;
+        if (frame.empty()) {  // auto rewind -- movie file playback only
+            capture.set(CV_CAP_PROP_POS_FRAMES, 0);
+            capture >> frame;
+        }
+        
+        glBindTexture(GL_TEXTURE_RECTANGLE, Texture);
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_BGR, GL_UNSIGNED_BYTE, frame.data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+        
+        // Clear the screen
+        glClear( GL_COLOR_BUFFER_BIT );
+        
+        // Use our shader
+        glUseProgram(programID);
+        
+        // Bind our texture in Texture Unit 0
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, Texture);
+        // Set our "myTextureSampler" sampler to user Texture Unit 0
+        glUniform1i(TextureID, 0);
+        
+        
+        // 1rst attribute buffer : vertices
+        glEnableVertexAttribArray(0);
+        glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+        glVertexAttribPointer(
+                              0,                  // attribute 0. No particular reason for 0, but must match the layout in the shader.
+                              3,                  // size  was 3
+                              GL_FLOAT,           // type
+                              GL_FALSE,           // normalized?
+                              0,                  // stride
+                              (void*)0            // array buffer offset
+                              );
+        
+        // 2nd attribute buffer : UVs
+        glEnableVertexAttribArray(1);
+        glBindBuffer(GL_ARRAY_BUFFER, uvbuffer);
+        glVertexAttribPointer(
+                              1,                                // attribute. No particular reason for 1, but must match the layout in the shader.
+                              2,                                // size : U+V => 2
+                              GL_FLOAT,                         // type
+                              GL_FALSE,                         // normalized?
+                              0,                                // stride
+                              (void*)0                          // array buffer offset
+                              );
+        
+        
+        // Draw the triangle !
+        glDrawArrays(GL_TRIANGLE_FAN, 0, 4); // 4 indices starting at 0 -> 1 rectangle
+        //glDrawArrays(GL_TRIANGLES, 0, 3); // 4 indices starting at 0 -> 1 rectangle
+        
+        glDisableVertexAttribArray(0);
+        glDisableVertexAttribArray(1);
+        
+        // Swap buffers
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+        
+    } // Check if the ESC key was pressed or the window was closed
+    while( glfwGetKey(window, GLFW_KEY_ESCAPE ) != GLFW_PRESS &&
+          glfwWindowShouldClose(window) == 0 );
+    
+    // Cleanup VBO and shader
+    glDeleteBuffers(1, &vertexbuffer);
+    glDeleteBuffers(1, &uvbuffer);
+    glDeleteProgram(programID);
+    glDeleteTextures(1, &TextureID);
+    glDeleteVertexArrays(1, &VertexArrayID);
+    
+    // Close OpenGL window and terminate GLFW
+    glfwTerminate();
     
     return 0;
 }
