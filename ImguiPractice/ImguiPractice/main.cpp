77029f775532b6a5583ad4553b8c08b0643852aa
Naoki Morita <namorit@gmail.com>
2016-12-02 02:34:27 +0900

    Add esc handling

--- ImguiPractice/ImguiPractice/main.cpp
+++ ImguiPractice/ImguiPractice/main.cpp
@@ -7,23 +7,38 @@
 //
 
 #include <iostream>
+#include <string>
 #include <opencv2/opencv.hpp>
 
 using namespace std;
 using namespace cv;
 
+void onMouse( int event, int x, int y, int flag, void* )
+{
+    
+}
+
+string win_name = "Window";
+
 int main(int argc, const char * argv[]) {
     
     VideoCapture cap(0);
     
-    namedWindow("Window");
+    namedWindow(win_name);
     
     while (1) {
         Mat frame;
         cap >> frame;
         
         imshow("Window", frame);
+        
+        int key = waitKey(1);
+        if(key==27) {
+            break;
+        }
     }
+    
+    destroyWindow(win_name);
     
     return 0;
 }
